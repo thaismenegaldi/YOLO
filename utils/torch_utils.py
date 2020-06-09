2003635b7109ac78c21503e8c0db5f9227aec756
Pedro Vinícius <pedbrgs@gmail.com>
2020-06-09 17:06:18 -0300

    Update on model_info method

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -112,7 +112,7 @@ def model_info(model, verbose=False):
     except:
         fs = ''
 
-    print('Model Summary: %g layers, %g parameters, %g gradients%s' % (len(list(model.parameters())), n_p, n_g, fs))
+    print('Model Summary: %d layers, %d parameters, %d gradients%s' % (len(list(model.parameters())), n_p, n_g, fs))
 
 
 def load_classifier(name='resnet101', n=2):
