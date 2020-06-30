6db2cde5ace25042a77231190b24418ee344ab0e
Pedro Vinícius <pedbrgs@gmail.com>
2020-06-30 15:24:03 -0300

    Bug fix in class label matrix method

--- pruning/prune.py
+++ pruning/prune.py
@@ -498,7 +498,6 @@ def class_label_matrix(labels, num_classes = 2):
             Y.append(1)
 
     # Converts a class vector (integers) to binary class matrix
-    Y = torch.eye(num_classes)
-    Y = Y[labels].numpy()
+    Y = np.eye(num_classes, dtype = 'uint8')[Y]
 
     return Y
