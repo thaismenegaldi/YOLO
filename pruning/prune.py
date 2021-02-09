a1d7d05d8f23a5fe25148251b542d157a2a01d05
Pedro Vinícius <pedbrgs@gmail.com>
2021-02-09 10:06:54 -0300

    Bug fix in YOLOv4 pruning

--- pruning/prune.py
+++ pruning/prune.py
@@ -255,13 +255,13 @@ def single_pruning_v4(model, block, filter):
     # Name of the current layer
     current_layer = str(model.module_list[block][0]).split('(')[0]
     # Name of the two next layers
-    if len(model.module_list[block+1]) > 1:
+    try:
         next_layer1 = str(model.module_list[block+1][0]).split('(')[0]
-    else:
+    except:
         next_layer1 = str(model.module_list[block+1]).split('(')[0]
-    if len(model.module_list[block+2]) > 1:
+    try:
         next_layer2 = str(model.module_list[block+2][0]).split('(')[0]
-    else:
+    except:
         next_layer2 = str(model.module_list[block+2]).split('(')[0]
 
     # Get information from the current convolutional layer
